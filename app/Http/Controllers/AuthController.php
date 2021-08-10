c456ede5dac5094cd8a76d89386bb2cf7006f022
andysarbini <kangandy09@gmail.com>
2021-08-10 17:00:49 +0700

    endpoint register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,7 +65,44 @@ class AuthController extends Controller
 
     public function register(Request $_request)
     {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
 
+            $status = "error";
+            $message = "";
+            $data = null;
+            $code = 400;
+            if ($validator->fails()) {
+                $errors = $validator->errors();
+                $message = $errors;
+            }
+            else {
+                $user = \App\User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                    'roles' => json_encode(['CUSTOMER']),
+                ]);
+                if($user) {
+                    $user->generateToken();
+                    $status = "success";
+                    $message = "register successfully";
+                    $data = $user->toArray();
+                    $code = 200;
+                }
+                else {
+                    $message = 'register failed';
+                }
+            }
+
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $code);
     }
 
     public function logout(Request $request)
